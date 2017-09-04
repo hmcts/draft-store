f6328a557e916c050d1e0f7e2cc6d3cad8da11f2
Rafal Kondratowicz <R.Kondratowicz@kainos.com>
2017-09-04 16:39:52 +0100

    Remove ErrorResult builder

--- src/main/java/uk/gov/hmcts/reform/draftstore/endpoint/domain/ErrorResult.java
+++ src/main/java/uk/gov/hmcts/reform/draftstore/endpoint/domain/ErrorResult.java
@@ -1,47 +1,17 @@
 package uk.gov.hmcts.reform.draftstore.endpoint.domain;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ErrorResult {
-    private ErrorCode errorCode;
-    private List<String> errors = new ArrayList<>();
 
-    public ErrorCode getErrorCode() {
-        return errorCode;
-    }
+    public final ErrorCode errorCode;
+    public final List<String> errors;
 
-    public List<String> getErrors() {
-        return errors;
-    }
-
-    public static class Builder {
-        private ErrorCode errorCode;
-        private List<String> errors;
-
-        public static Builder errorResultBuilder(ErrorCode errorCode) {
-            Builder builder = new Builder();
-            builder.errorCode = errorCode;
-            builder.errors = new ArrayList<>();
-            return builder;
-        }
-
-        public Builder withError(String error) {
-            if (!StringUtils.isEmpty(error)) {
-                errors.add(error);
-            }
-            return this;
-        }
-
-        public ErrorResult build() {
-            ErrorResult instance = new ErrorResult();
-            instance.errorCode = errorCode;
-            instance.errors = errors;
-            return instance;
-        }
+    public ErrorResult(ErrorCode errorCode, List<String> errors) {
+        this.errorCode = errorCode;
+        this.errors = errors;
     }
 }
