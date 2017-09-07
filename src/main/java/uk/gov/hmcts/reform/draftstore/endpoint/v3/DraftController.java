8f35b4fb44c9fb1fec6a6efeed7c6e40d71a6ab6
Rafal Kondratowicz <R.Kondratowicz@kainos.com>
2017-09-07 09:56:17 +0100

    Add new controller action for creating drafts

--- src/main/java/uk/gov/hmcts/reform/draftstore/endpoint/v3/DraftController.java
+++ src/main/java/uk/gov/hmcts/reform/draftstore/endpoint/v3/DraftController.java
@@ -1,20 +1,27 @@
 package uk.gov.hmcts.reform.draftstore.endpoint.v3;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.draftstore.data.DraftStoreDAO;
+import uk.gov.hmcts.reform.draftstore.domain.CreateDraft;
 import uk.gov.hmcts.reform.draftstore.domain.Draft;
 import uk.gov.hmcts.reform.draftstore.exception.NoDraftFoundException;
 import uk.gov.hmcts.reform.draftstore.service.UserIdentificationService;
 
 import java.util.List;
+import javax.validation.Valid;
 import java.util.Objects;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.ResponseEntity.status;
 
 @RestController
 @RequestMapping("drafts")
@@ -47,5 +54,16 @@ public class DraftController {
     ) {
         String currentUserId = userIdService.userIdFromAuthToken(authHeader);
         return draftRepo.readAll(currentUserId, type);
+    }
+
+    @PostMapping
+    public ResponseEntity create(
+        @RequestHeader(AUTHORIZATION) String authHeader,
+        @RequestBody @Valid CreateDraft newDraft
+    ) {
+        String currentUserId = userIdService.userIdFromAuthToken(authHeader);
+        draftRepo.insert(currentUserId, newDraft);
+
+        return status(HttpStatus.CREATED).build();
     }
 }
