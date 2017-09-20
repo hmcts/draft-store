d9a610c0b51757ab61b00c0672fb90850548b2c6
Rafal Kondratowicz <rafalko@lemon.kainos.com>
2017-09-20 09:46:44 +0100

    Remove old comment

--- src/main/java/uk/gov/hmcts/reform/draftstore/service/AuthService.java
+++ src/main/java/uk/gov/hmcts/reform/draftstore/service/AuthService.java
@@ -4,18 +4,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import uk.gov.hmcts.reform.draftstore.exception.AuthorizationException;
 import uk.gov.hmcts.reform.draftstore.service.idam.IdamClient;
-import uk.gov.hmcts.reform.draftstore.service.idam.User;
 
 import java.util.Optional;
 import javax.validation.constraints.NotNull;
 
-/*
-TODO: implementation of this service should be replaced with a request to the IDAM /details service (to get the userid)
-The auth header should have a Bearer JWT token.
-For tactical reasons, no IDAM stubs/not deployed anywhere, Divorce BA's decided to go with this solution.
-Initially the implementation did indeed send a request to IDAM's /details endpoint to get the documenter's user id
-@see http://git.reform/divorce/draft-document-store/commit/91839f84e54e42f5b70f28c68a150ad60ef86b9e
- */
 @Service
 public class AuthService {
 
