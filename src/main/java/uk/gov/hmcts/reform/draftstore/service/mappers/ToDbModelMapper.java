1b1b02e9e0757e5e060d742494f7ccf45a5c3cb8
Rafal Kondratowicz <rafalko@lemon.kainos.com>
2017-10-12 19:23:17 +0100

    Simplify

--- src/main/java/uk/gov/hmcts/reform/draftstore/service/mappers/ToDbModelMapper.java
+++ src/main/java/uk/gov/hmcts/reform/draftstore/service/mappers/ToDbModelMapper.java
@@ -3,8 +3,6 @@ package uk.gov.hmcts.reform.draftstore.service.mappers;
 import uk.gov.hmcts.reform.draftstore.data.model.CreateDraft;
 import uk.gov.hmcts.reform.draftstore.data.model.UpdateDraft;
 
-import java.util.function.Supplier;
-
 import static uk.gov.hmcts.reform.draftstore.service.crypto.CryptoService.encrypt;
 
 public class ToDbModelMapper {
@@ -13,11 +11,9 @@ public class ToDbModelMapper {
         uk.gov.hmcts.reform.draftstore.domain.CreateDraft draft,
         String secret
     ) {
-        Docs docs = getDocuments(secret, () -> draft.document.toString());
-
         return new CreateDraft(
-            docs.plainText,
-            docs.encrypted,
+            secret == null ? draft.document.toString() : null,
+            secret == null ? null : encrypt(draft.document.toString(), secret),
             draft.type,
             draft.maxStaleDays
         );
@@ -27,28 +23,10 @@ public class ToDbModelMapper {
         uk.gov.hmcts.reform.draftstore.domain.UpdateDraft draft,
         String secret
     ) {
-        Docs docs = getDocuments(secret, () -> draft.document.toString());
-
         return new UpdateDraft(
-            docs.plainText,
-            docs.encrypted,
+            secret == null ? draft.document.toString() : null,
+            secret == null ? null : encrypt(draft.document.toString(), secret),
             draft.type
         );
-    }
-
-    private static Docs getDocuments(String secret, Supplier<String> inputDocument) {
-        return secret != null
-            ? new Docs(null, encrypt(inputDocument.get(), secret))
-            : new Docs(inputDocument.get(), null);
-    }
-
-    static class Docs {
-        String plainText;
-        byte[] encrypted;
-
-        public Docs(String plainText, byte[] encrypted) {
-            this.plainText = plainText;
-            this.encrypted = encrypted;
-        }
     }
 }
