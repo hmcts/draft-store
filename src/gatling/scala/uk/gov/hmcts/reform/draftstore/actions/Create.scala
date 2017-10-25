e3c9de2ccd5b5eacd896fe81113c85cfa29164ac
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2017-10-25 14:28:42 +0100

    Encrypt drafts in gatling tests

--- src/gatling/scala/uk/gov/hmcts/reform/draftstore/actions/Create.scala
+++ src/gatling/scala/uk/gov/hmcts/reform/draftstore/actions/Create.scala
@@ -1,36 +1,42 @@
 package uk.gov.hmcts.reform.draftstore.actions
 
+import java.util.UUID
+
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef._
 
 object Create {
 
+  private val secretFeeder = Iterator.continually(Map("secret" -> UUID.randomUUID.toString))
+
   val create: ChainBuilder =
-    exec(
-      http("Create draft")
-        .post(url = "")
-        .headers(Map(
-          "ServiceAuthorization" -> "Bearer ${service_token}",
-          "Authorization" -> "Bearer ${user_token}"
-        ))
-        .body(
-          StringBody(
-            """
-              |{
-              |  "document": {
-              |    "a": "some value",
-              |    "b": "some other value",
-              |    "c": "yet another",
-              |    "nested": {
-              |      "xxx": "yyy"
-              |    }
-              |  },
-              |  "type": "my type"
-              |}
-            """.stripMargin
+    feed(secretFeeder)
+      .exec(
+        http("Create draft")
+          .post(url = "")
+          .headers(Map(
+            "ServiceAuthorization" -> "Bearer ${service_token}",
+            "Authorization" -> "Bearer ${user_token}",
+            "Secret" -> "${secret}"
+          ))
+          .body(
+            StringBody(
+              """
+                |{
+                |  "document": {
+                |    "a": "some value",
+                |    "b": "some other value",
+                |    "c": "yet another",
+                |    "nested": {
+                |      "xxx": "yyy"
+                |    }
+                |  },
+                |  "type": "my type"
+                |}
+              """.stripMargin
+            )
           )
-        )
-        .check(headerRegex("Location", """/(\d+)$""").saveAs("id"))
-    )
+          .check(headerRegex("Location", """/(\d+)$""").saveAs("id"))
+      )
 }
