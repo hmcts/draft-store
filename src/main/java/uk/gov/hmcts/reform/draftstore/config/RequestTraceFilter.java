4f63ccfc9f30f261e7cbdf6dc3adc0e744332602
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2017-11-08 15:04:09 +0000

    Don't trace sensitive headers

---
+++ src/main/java/uk/gov/hmcts/reform/draftstore/config/RequestTraceFilter.java
@@ -0,0 +1,44 @@
+package uk.gov.hmcts.reform.draftstore.config;
+
+import org.springframework.boot.actuate.trace.TraceProperties;
+import org.springframework.boot.actuate.trace.TraceRepository;
+import org.springframework.boot.actuate.trace.WebRequestTraceFilter;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toSet;
+import static uk.gov.hmcts.reform.draftstore.service.AuthService.SECRET_HEADER;
+import static uk.gov.hmcts.reform.draftstore.service.AuthService.SERVICE_HEADER;
+
+@Component
+public class RequestTraceFilter extends WebRequestTraceFilter {
+
+    public final static List<String> sensitiveHeaders =
+        Arrays.asList(
+            SECRET_HEADER,
+            SERVICE_HEADER
+        );
+
+    public RequestTraceFilter(TraceRepository repository, TraceProperties properties) {
+        super(repository, properties);
+    }
+
+    @Override
+    protected void postProcessRequestHeaders(Map<String, Object> headers) {
+        headers
+            .keySet()
+            .stream()
+            .filter(header -> shouldBeRemoved(header))
+            .collect(toSet())
+            .forEach(h -> headers.remove(h));
+    }
+
+    private boolean shouldBeRemoved(String header) {
+        return sensitiveHeaders
+            .stream()
+            .anyMatch(sensitiveHeader -> header.equalsIgnoreCase(sensitiveHeader));
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/draftstore/config/RequestTraceFilterTest.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.draftstore.config;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.draftstore.service.AuthService.SECRET_HEADER;
+import static uk.gov.hmcts.reform.draftstore.service.AuthService.SERVICE_HEADER;
+
+public class RequestTraceFilterTest {
+
+    @Test
+    public void should_filter_out_sensitive_headers() throws Exception {
+
+        Map<String, Object> headers = new HashMap<>();
+        headers.put(SECRET_HEADER, "xxx");
+        headers.put(SECRET_HEADER.toLowerCase(), "xxx");
+        headers.put(SERVICE_HEADER, "xxx");
+        headers.put(SERVICE_HEADER.toLowerCase(), "xxx");
+        headers.put("a", "xxx");
+        headers.put("b", "xxx");
+        headers.put("c", "xxx");
+
+        RequestTraceFilter filter = new RequestTraceFilter(null, null);
+        filter.postProcessRequestHeaders(headers);
+
+        assertThat(headers.keySet())
+            .as("remaining headers")
+            .containsExactly("a", "b", "c");
+    }
+}
