600b9d5355eb5ee0dae477d91a0a23203468106e
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2017-11-13 17:07:28 +0000

    Cache results

--- src/main/java/uk/gov/hmcts/reform/draftstore/info/UnencryptedDraftsInfoContributor.java
+++ src/main/java/uk/gov/hmcts/reform/draftstore/info/UnencryptedDraftsInfoContributor.java
@@ -5,10 +5,21 @@ import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.draftstore.data.DraftStoreDAO;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.time.LocalDateTime.now;
+
 @Component
 public class UnencryptedDraftsInfoContributor implements InfoContributor {
 
     private final DraftStoreDAO repo;
+
+    private LocalDateTime lastCheckDate = null;
+    private List<Map<String, Object>> lastResult = new ArrayList<>();
 
     public UnencryptedDraftsInfoContributor(DraftStoreDAO repo) {
         this.repo = repo;
@@ -16,6 +27,18 @@ public class UnencryptedDraftsInfoContributor implements InfoContributor {
 
     @Override
     public void contribute(Info.Builder builder) {
-        builder.withDetail("unecrypted_drafts", repo.getUnencryptedDrafts());
+        if (lastResult.isEmpty() || shouldRefresh()) {
+            this.lastResult = repo.getUnencryptedDrafts();
+            this.lastCheckDate = now();
+        }
+
+        builder.withDetail("unencrypted_drafts", this.lastResult);
+    }
+
+    private boolean shouldRefresh() {
+        return Optional
+            .ofNullable(lastCheckDate)
+            .map(x -> x.plusMinutes(1).isBefore(now()))
+            .orElse(true);
     }
 }
