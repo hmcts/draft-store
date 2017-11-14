2ebc345f6de117f972a49fb99da1379c9ea78ed8
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2017-11-14 11:01:15 +0000

    Add info on unencrypted drafts (#80)

---
+++ src/main/java/uk/gov/hmcts/reform/draftstore/info/UnencryptedDraftsInfoContributor.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.draftstore.info;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.draftstore.data.DraftStoreDAO;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static java.time.LocalDateTime.now;
+
+@Component
+public class UnencryptedDraftsInfoContributor implements InfoContributor {
+
+    private final DraftStoreDAO repo;
+
+    private LocalDateTime lastCheckDate = null;
+    private List<Map<String, Object>> lastResult = new ArrayList<>();
+
+    public UnencryptedDraftsInfoContributor(DraftStoreDAO repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        if (lastCheckDate == null || Duration.between(lastCheckDate, now()).getSeconds() >= 60) {
+            this.lastResult = repo.getUnencryptedDrafts();
+            this.lastCheckDate = now();
+        }
+
+        builder.withDetail("unencrypted_drafts", this.lastResult);
+    }
+}
