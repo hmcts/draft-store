65141a432858c6e9ac2e83f20c1fe66408a11e4f
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-01-10 13:32:24 +0000

    cnp infrastructure (#101)

---
+++ infrastructure/main.tf
@@ -0,0 +1,33 @@
+resource "random_string" "password" {
+  length = 32
+  special = true
+}
+
+module "api" {
+  source   = "git@github.com/hmcts/terraform-module-webapp.git"
+  product  = "${var.product}-api"
+  location = "${var.location_api}"
+  env      = "${var.env}"
+  ilbIp    = "${var.ilbIp}"
+
+  app_settings = {
+    DRAFT_STORE_DB_HOST     = "${module.db.host_name}"
+    DRAFT_STORE_DB_PORT     = "${module.db.postgresql_listen_port}"
+    DRAFT_STORE_DB_PASSWORD = "${random_string.password.result}"
+
+    IDAM_URL                = "http://betaDevBccidamAppLB.reform.hmcts.net:4551"
+    S2S_URL                 = "http://betaDevBccidamAppLB.reform.hmcts.net:4552"
+
+    MAX_STALE_DAYS_DEFAULT  = "${var.max_stale_days_default}"
+    MAX_STALE_DAYS_CRON     = "${var.max_stale_days_cron}"
+  }
+}
+
+module "db" {
+  source              = "git@github.com/hmcts/terraform-module-postgres.git"
+  product             = "${var.product}-db"
+  location            = "${var.location_db}"
+  env                 = "${var.env}"
+  postgresql_user     = "draftstore"
+  postgresql_password = "${random_string.password.result}"
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,32 @@
+variable "product" {
+  type    = "string"
+  default = "draft-store"
+}
+
+variable "location_api" {
+  type    = "string"
+  default = "UK South"
+}
+
+variable "location_db" {
+  type    = "string"
+  default = "West Europe"
+}
+
+variable "env" {
+  type = "string"
+}
+
+variable "ilbIp" {}
+
+# region app config
+
+variable "max_stale_days_default" {
+  default = 90
+}
+
+variable "max_stale_days_cron" {
+  default = "0 0 3 * * *"
+}
+
+# endregion
