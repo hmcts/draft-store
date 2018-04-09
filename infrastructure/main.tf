90ddfc37577569c83a573e2271e692bb3ec9aef2
leszekg <gonczar@gmail.com>
2018-04-09 14:53:13 +0100

    Fix infrastructure code for postgres DB

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,9 @@
-resource "random_string" "password" {
-  length = 32
-  special = true
+module "db" {
+  source              = "git@github.com:hmcts/moj-module-postgres"
+  product             = "${var.product}-db"
+  location            = "${var.location_db}"
+  env                 = "${var.env}"
+  postgresql_user     = "draftstore"
 }
 
 module "api" {
@@ -13,7 +16,7 @@ module "api" {
   app_settings = {
     DRAFT_STORE_DB_HOST     = "${module.db.host_name}"
     DRAFT_STORE_DB_PORT     = "${module.db.postgresql_listen_port}"
-    DRAFT_STORE_DB_PASSWORD = "${random_string.password.result}"
+    DRAFT_STORE_DB_PASSWORD = "${module.db.postgresql_password}"
 
     IDAM_URL                = "http://betaDevBccidamAppLB.reform.hmcts.net:4551"
     S2S_URL                 = "http://betaDevBccidamAppLB.reform.hmcts.net:4552"
@@ -21,13 +24,4 @@ module "api" {
     MAX_STALE_DAYS_DEFAULT  = "${var.max_stale_days_default}"
     MAX_STALE_DAYS_CRON     = "${var.max_stale_days_cron}"
   }
-}
-
-module "db" {
-  source              = "git@github.com:hmcts/moj-module-postgres"
-  product             = "${var.product}-db"
-  location            = "${var.location_db}"
-  env                 = "${var.env}"
-  postgresql_user     = "draftstore"
-  postgresql_password = "${random_string.password.result}"
 }
