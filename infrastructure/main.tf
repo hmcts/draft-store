4efd21ae603f13ad321918c75492eb3f05072e0e
leszekg <gonczar@gmail.com>
2018-04-12 12:07:03 +0100

    Make the app talk to DB using the right options

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,15 +19,18 @@ module "api" {
   subscription  = "${var.subscription}"
 
   app_settings = {
-    DRAFT_STORE_DB_HOST     = "${module.db.host_name}"
-    DRAFT_STORE_DB_PORT     = "${module.db.postgresql_listen_port}"
-    DRAFT_STORE_DB_PASSWORD = "${module.db.postgresql_password}"
+    DRAFT_STORE_DB_HOST         = "${module.db.host_name}"
+    DRAFT_STORE_DB_PORT         = "${module.db.postgresql_listen_port}"
+    DRAFT_STORE_DB_PASSWORD     = "${module.db.postgresql_password}"
+    DRAFT_STORE_DB_USER_NAME    = "${module.db.user_name}"
+    DRAFT_STORE_DB_NAME         = "${module.db.postgresql_database}"
+    DRAFT_STORE_DB_CONN_OPTIONS = "${local.db_connection_options}"
 
-    IDAM_URL                = "http://betaDevBccidamAppLB.reform.hmcts.net:4551"
-    S2S_URL                 = "http://betaDevBccidamAppLB.reform.hmcts.net:4552"
+    IDAM_URL                    = "http://betaDevBccidamAppLB.reform.hmcts.net:4551"
+    S2S_URL                     = "http://betaDevBccidamAppLB.reform.hmcts.net:4552"
 
-    MAX_STALE_DAYS_DEFAULT  = "${var.max_stale_days_default}"
-    MAX_STALE_DAYS_CRON     = "${var.max_stale_days_cron}"
+    MAX_STALE_DAYS_DEFAULT      = "${var.max_stale_days_default}"
+    MAX_STALE_DAYS_CRON         = "${var.max_stale_days_cron}"
 
     FLYWAY_URL                  = "jdbc:postgresql://${module.db.host_name}:${module.db.postgresql_listen_port}/${module.db.postgresql_database}${local.db_connection_options}"
     FLYWAY_USER                 = "${module.db.user_name}"
