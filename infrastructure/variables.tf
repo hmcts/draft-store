4201291aab5d911a027a4760d9498db132042f5a
leszekg <gonczar@gmail.com>
2018-04-12 12:01:51 +0100

    Enable DB migrations in the pipeline

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -23,6 +23,17 @@ variable "env" {
 
 variable "ilbIp" {}
 
+variable "tenant_id" {}
+
+variable "client_id" {
+  description = "(Required) The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies. This is usually sourced from environment variables and not normally required to be specified."
+}
+
+variable "jenkins_AAD_objectId" {
+  type        = "string"
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
 variable "subscription" {}
 
 # region app config
