4a962754e93e146301811af7537480ff7dfc246b
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-04-13 14:08:24 +0100

    Add basic functional test (#128)
    Functional tests treat tested application as a black box: feeding data
    and evaluating results.
    
    Here is the first, basic test that expects HTTP status code 400 when
    missing required headers.
    
    Application URL is configurable either with application.properties or
    TEST_URL environment variable.

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/draftstore/features/security/AuthenticatedRequestTest.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.reform.draftstore.features.security;
+
+import io.restassured.RestAssured;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@TestPropertySource("classpath:application.properties")
+@RunWith(SpringRunner.class)
+public class AuthenticatedRequestTest {
+
+    @Value("${test-url}")
+    private String draftStoreUrl;
+
+    @Test
+    public void should_reject_request_when_missing_required_headers() {
+        RestAssured
+            .given()
+            .log().all()
+            .relaxedHTTPSValidation()
+            .baseUri(draftStoreUrl)
+            .basePath("/drafts")
+            .queryParam("type", "default")
+            .when()
+            .get()
+            .then()
+            .assertThat()
+            .statusCode(400);
+    }
+}
