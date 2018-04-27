7788ff1e83da2f9fd41f77a250d5236956ad6089
leszekg <gonczar@gmail.com>
2018-04-27 15:53:17 +0100

    Simplify Vault name configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,31 +3,38 @@ provider "vault" {
 }
 
 locals {
-  db_connection_options      = "?ssl=true"
-  ase_name                   = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  db_connection_options  = "?ssl=true"
+  ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
 
   # Environment we take our dependencies from. Typically the same as for the app,
   # but has to be different for preview
-  dependencies_env = "${var.env == "preview"
-                          ? "aat"
-                          : var.env == "spreview" ? "saat" : var.env
-                      }"
+  dependencies_env       = "${var.env == "preview"
+                                ? "aat"
+                                : var.env == "spreview" ? "saat" : var.env
+                             }"
 
-  dependencies_ase = "${var.env == "preview"
-                          ? "core-compute-aat"
-                          : var.env == "spreview" ? "core-compute-saat" : local.ase_name
-                       }"
+  dependencies_ase       = "${var.env == "preview"
+                                ? "core-compute-aat"
+                                : var.env == "spreview" ? "core-compute-saat" : local.ase_name
+                             }"
 
-  s2s_url         = "http://rpe-service-auth-provider-${local.dependencies_env}.service.${local.dependencies_ase}.internal"
-  max_vault_name_len         = 24
-  max_vault_product_name_len = "${var.deployment_namespace != ""
-                                    ? local.max_vault_name_len
-                                    : local.max_vault_name_len - 1 - length(var.env)}"
+  s2s_url                = "http://rpe-service-auth-provider-${local.dependencies_env}.service.${local.dependencies_ase}.internal"
 
-  vault_product_name         = "${substr(var.product, 0, min(length(var.product), local.max_vault_product_name_len))}"
-  vault_name                 = "${var.deployment_namespace != ""
-                                    ? local.vault_product_name
-                                    : format("%s-%s", local.vault_product_name, var.env)}"
+  preview_vault_name     = "draft-store-aat"
+  default_vault_name     = "${var.product}-${var.env}"
+  vault_name             = "${(var.env == "preview" || var.env == "spreview")
+                                 ? local.preview_vault_name
+                                 : local.default_vault_name
+                             }"
+
+  non_preview_vault_uri  = "${module.key-vault.key_vault_uri}"
+
+  # use aat vault so that there's no need to populate it with data every time a PR is created
+  preview_vault_uri      = "https://draft-store-aat.vault.azure.net/"
+  vault_uri              = "${(var.env == "preview" || var.env == "spreview")
+                                 ? local.preview_vault_uri
+                                 : local.non_preview_vault_uri
+                             }"
 }
 
 module "db" {
@@ -72,6 +79,9 @@ module "api" {
 }
 
 # region save DB details to Azure Key Vault
+
+# this key vault is created in every environment, but preview, being short-lived,
+# will use the aat one instead
 module "key-vault" {
   source              = "git@github.com:hmcts/moj-module-key-vault?ref=master"
   name                = "${local.vault_name}"
@@ -82,35 +92,36 @@ module "key-vault" {
   resource_group_name = "${module.api.resource_group_name}"
   # dcd_cc-dev group object ID
   product_group_object_id = "38f9dea6-e861-4a50-9e73-21e64f563537"
+  count               = "${(var.env == "preview" || var.env == "spreview") ? 0 : 1}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
   name      = "${var.component}-POSTGRES-USER"
   value     = "${module.db.user_name}"
-  vault_uri = "${module.key-vault.key_vault_uri}"
+  vault_uri = "${local.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name      = "${var.component}-POSTGRES-PASS"
   value     = "${module.db.postgresql_password}"
-  vault_uri = "${module.key-vault.key_vault_uri}"
+  vault_uri = "${local.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
   name      = "${var.component}-POSTGRES-HOST"
   value     = "${module.db.host_name}"
-  vault_uri = "${module.key-vault.key_vault_uri}"
+  vault_uri = "${local.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
   name      = "${var.component}-POSTGRES-PORT"
   value     = "${module.db.postgresql_listen_port}"
-  vault_uri = "${module.key-vault.key_vault_uri}"
+  vault_uri = "${local.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
   name      = "${var.component}-POSTGRES-DATABASE"
   value     = "${module.db.postgresql_database}"
-  vault_uri = "${module.key-vault.key_vault_uri}"
+  vault_uri = "${local.vault_uri}"
 }
 # endregion
