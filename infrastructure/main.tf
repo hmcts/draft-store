c78daab2621f2a7cad60cd249926ddb4f0f29cd4
Kevin Cross <kevin.j.cross@gmail.com>
2019-02-14 16:20:03 +0000

    terraform version pin (#264)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  version = "1.21"
+}
+
 locals {
   db_connection_options = "?sslmode=require"
   ase_name              = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
