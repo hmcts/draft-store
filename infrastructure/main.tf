993bd4a4cb1cc280edc4b0f61bb51f50d59c6d5b
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2019-03-27 11:36:43 +0000

    Update main.tf (#276)
    Added common_tags  to key vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -68,6 +68,7 @@ module "key-vault" {
 
   # dcd_cc-dev group object ID
   product_group_object_id = "38f9dea6-e861-4a50-9e73-21e64f563537"
+  common_tags         = "${var.common_tags}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
