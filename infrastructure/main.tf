e70097d7cf24e565073434f68b30e7c80a83306c
kunlebalogun <alliu.balogun@hackney.gov.uk>
2021-03-30 12:29:09 +0100

    Added old DB and vault module in addition to v11

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,7 +13,7 @@ data "azurerm_subnet" "postgres" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-v11-${var.env}"
+  name     = "${var.product}-${var.component}-${var.env}"
   location = var.location
 
   tags = var.common_tags
@@ -73,6 +73,38 @@ module "key-vault" {
   common_tags                = "${var.common_tags}"
   managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.rpe-shared-identity.principal_id}"]
 }
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name         = "${var.component}-POSTGRES-USER"
+  value        = module.db.user_name
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name         = "${var.component}-POSTGRES-PASS"
+  value        = module.db.postgresql_password
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name         = "${var.component}-POSTGRES-HOST"
+  value        = module.db.host_name
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name         = "${var.component}-POSTGRES-PORT"
+  value        = "5432"
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name         = "${var.component}-POSTGRES-DATABASE"
+  value        = module.db.postgresql_database
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+## PostgreSQL v11 
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER-V11" {
   name         = "${var.component}-POSTGRES-USER-V11"
