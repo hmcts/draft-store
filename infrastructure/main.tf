d98be38cde4c967958e85e510441198c5c9eaf05
Thomas Thornton <>
2021-04-12 14:17:12 +0100

    Remove duplicate

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -35,12 +35,6 @@ module "db" {
   subscription       = var.subscription
 }
 
-data "azurerm_subnet" "postgres" {
-  name                 = "core-infra-subnet-0-${var.env}"
-  resource_group_name  = "core-infra-${var.env}"
-  virtual_network_name = "core-infra-vnet-${var.env}"
-}
-
 ## PostgreSQL v11
 module "db-v11" {
   source             = "git@github.com:hmcts/cnp-module-postgres?ref=postgresql_tf"
