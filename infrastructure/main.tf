7a3f73508b2e572ace0a57db2da427d3e8e0b1e7
Thomas Thornton <>
2023-01-26 09:43:40 +0000

    add provider for cft_vnet to test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,8 +2,39 @@ provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  subscription_id            = local.mi_cft[var.env].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "cft_vnet"
+}
+
 locals {
   s2s_url = "http://rpe-service-auth-provider-${var.env}.service.core-compute-${var.env}.internal"
+
+  cft_vnet = {
+    sbox = {
+      subscription = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
+    }
+    perftest = {
+      subscription = "8a07fdcd-6abd-48b3-ad88-ff737a4b9e3c"
+    }
+    aat = {
+      subscription = "96c274ce-846d-4e48-89a7-d528432298a7"
+    }
+    ithc = {
+      subscription = "62864d44-5da9-4ae9-89e7-0cf33942fa09"
+    }
+    preview = {
+      subscription = "8b6ea922-0862-443e-af15-6056e1c9b9a4"
+    }
+    prod = {
+      subscription = "8cbc6f36-7c56-4963-9d36-739db5d00b27"
+    }
+    demo = {
+      subscription = "d025fece-ce99-4df2-b7a9-b649d3ff2060"
+    }
+  }
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -31,6 +62,7 @@ module "db" {
 
 # FlexibleServer v14
 data "azurerm_subnet" "postgres" {
+  provider             = azurerm.cft_vnet
   name                 = "postgresql"
   resource_group_name  = "cft-${var.env}-network-rg"
   virtual_network_name = "cft-${var.env}-vnet"
