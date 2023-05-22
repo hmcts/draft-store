49d71503a26402a3a16a56ac07d055955e8b6c2f
Matt Slater <72858053+msl8r@users.noreply.github.com>
2023-05-22 11:08:48 +0100

    DTSPO-13832 - remove single server (#1322)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -47,22 +47,6 @@ resource "azurerm_resource_group" "rg" {
   tags = var.common_tags
 }
 
-module "db" {
-  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product            = var.product
-  component          = var.component
-  name               = "rpe-${var.product}"
-  location           = var.location
-  env                = var.env
-  database_name      = "draftstore"
-  postgresql_user    = "draftstore"
-  postgresql_version = "10"
-  sku_name           = "GP_Gen5_2"
-  sku_tier           = "GeneralPurpose"
-  common_tags        = var.common_tags
-  subscription       = var.subscription
-}
-
 # FlexibleServer v14
 module "postgresql" {
   providers = {
@@ -111,36 +95,6 @@ module "key-vault" {
   product_group_object_id     = "38f9dea6-e861-4a50-9e73-21e64f563537"
   common_tags                 = var.common_tags
   managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.rpe-shared-identity.principal_id}"]
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  name         = "${var.component}-POSTGRES-USER"
-  value        = module.db.user_name
-  key_vault_id = module.key-vault.key_vault_id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-  name         = "${var.component}-POSTGRES-PASS"
-  value        = module.db.postgresql_password
-  key_vault_id = module.key-vault.key_vault_id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  name         = "${var.component}-POSTGRES-HOST"
-  value        = module.db.host_name
-  key_vault_id = module.key-vault.key_vault_id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-  name         = "${var.component}-POSTGRES-PORT"
-  value        = "5432"
-  key_vault_id = module.key-vault.key_vault_id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  name         = "${var.component}-POSTGRES-DATABASE"
-  value        = module.db.postgresql_database
-  key_vault_id = module.key-vault.key_vault_id
 }
 
 # FlexibleServer v14 creds
