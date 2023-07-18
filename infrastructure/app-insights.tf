239649e4c4ade2a21b6efa8ef3b26b7b9fe3dd35
Tim Jacomb <timjacomb1@gmail.com>
2023-07-18 15:08:01 +0100

    Typo

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -17,7 +17,7 @@ moved {
 
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
-  value        = azurerm_application_insights.appinsights.connection_string
+  value        = module.application_insights.connection_string
   key_vault_id = module.key-vault.key_vault_id
 }
 
