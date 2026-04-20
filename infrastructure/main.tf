1caf7bf0685f57583a2f1c3ffbe03a939a7994b1
manohar.nunna <manohar.nunna@hmcts.net>
2026-04-20 14:46:03 +0100

    DTSPO-30184: Add jenkins managed identity keyvault access

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -80,6 +80,11 @@ data "azurerm_user_assigned_identity" "rpe-shared-identity" {
   resource_group_name = "managed-identities-${var.env}-rg"
 }
 
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 # region save DB details to Azure Key Vault
 
 # this key vault is created in every environment, but preview, being short-lived,
@@ -90,6 +95,7 @@ module "key-vault" {
   env                 = var.env
   tenant_id           = var.tenant_id
   object_id           = var.jenkins_AAD_objectId
+  jenkins_object_id   = data.azurerm_user_assigned_identity.jenkins.principal_id
   resource_group_name = azurerm_resource_group.rg.name
 
   # dcd_cc-dev group object ID
